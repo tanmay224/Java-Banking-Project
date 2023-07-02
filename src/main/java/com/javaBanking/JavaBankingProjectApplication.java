ac7b7497a2646ffb13ee251d8259be6db3360818
tanmayjena <tanmayjena224@gmail.com>
2023-07-02 17:34:41 +0530

    new string added

--- src/main/java/com/javaBanking/JavaBankingProjectApplication.java
+++ src/main/java/com/javaBanking/JavaBankingProjectApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaBankingProjectApplication {
-
+static String a="hello world";
 	public static void main(String[] args) {
 		SpringApplication.run(JavaBankingProjectApplication.class, args);
+		System.out.println(a);
 	}
 
 }
