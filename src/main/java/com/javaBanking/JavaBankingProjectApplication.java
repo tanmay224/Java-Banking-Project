aeaf4b011752c0bb69dc4b35d0162ed19c9c7851
tanmay224 <136701987+tanmay224@users.noreply.github.com>
2023-07-02 17:32:19 +0530

    Update JavaBankingProjectApplication.java adding a = 10

--- src/main/java/com/javaBanking/JavaBankingProjectApplication.java
+++ src/main/java/com/javaBanking/JavaBankingProjectApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaBankingProjectApplication {
-
+static int a = 10;
 	public static void main(String[] args) {
 		SpringApplication.run(JavaBankingProjectApplication.class, args);
+		System.out.println(a);
 	}
 
 }
