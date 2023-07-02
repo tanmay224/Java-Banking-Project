e78a85dc1b8bd9055421457a3a7bd534cd260094
tanmayjena <tanmayjena224@gmail.com>
2023-07-02 12:44:51 +0530

    initial commit

---
+++ src/main/java/com/javaBanking/JavaBankingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.javaBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBankingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBankingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaBanking/JavaBankingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBankingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
